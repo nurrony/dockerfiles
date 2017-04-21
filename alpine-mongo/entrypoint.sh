62e4b50792375e73e0abce029fdef8019845339d
Nur Rony <pro.nmrony@gmail.com>
2017-04-22 04:25:26 +0600

    creates alpine-mongo

---
+++ alpine-mongo/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/dumb-init /bin/sh
+
+echo "starting MongoDB"
+
+mongod --port ${MONGO_PORT} --dbpath ${MONGO_DBPATH} --logpath ${MONGO_LOGFILE} --logappend -v &
+echo "please wait - live logging in 5 secs ..."
+sleep 5
+tail -f ${MONGO_LOGFILE}
