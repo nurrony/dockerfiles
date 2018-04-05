e7b18fc4d0abbdccc07568dcecd29d85dc001d0e
Nur Rony <pro.nmrony@gmail.com>
2018-04-05 23:22:12 +0600

    add drop in command

---
+++ awscli/aws-commands.sh
@@ -0,0 +1,20 @@
+# Copy and paste these functions in your ~/.bashrc or ~/.zshrc
+
+aws() {
+  docker run --rm \
+  -it \
+  --name aws-cli \
+  --volume $HOME/.aws:/root/.aws \
+  --user $(id -u):$(id -g) \
+  nmrony/awscli "$@"
+}
+
+eb() {
+  docker run --rm \
+    -t $(tty &>/dev/null && echo "-i") \
+    --user $(id -u):$(id -g) \
+    -v "$(pwd):/app" \
+    -v "${HOME}/.aws:/root/.aws" \
+    -v "${HOME}/.ssh:/root/.ssh" \
+    nmrony/awscli "$@"
+}
