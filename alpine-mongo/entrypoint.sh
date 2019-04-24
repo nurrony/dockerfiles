44ab3d8366aef7fbfa68efe26972451aef534d57
Nur Rony <pro.nmrony@gmail.com>
2019-04-24 15:46:47 +0600

    feat(build): make accpet any version through build arg

--- alpine-mongo/entrypoint.sh
+++ alpine-mongo/entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "starting MongoDB"
 
-mongod --port ${MONGO_PORT} --dbpath ${MONGO_DBPATH} --logpath ${MONGO_LOGFILE} --logappend -v &
+mongod --bind_ip_all --port ${MONGO_PORT} --dbpath ${MONGO_DBPATH} --logpath ${MONGO_LOGFILE} --logappend -v &
 echo "please wait - live logging in 5 secs ..."
 sleep 5
 tail -f ${MONGO_LOGFILE}
